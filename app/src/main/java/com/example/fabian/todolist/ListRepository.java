38bbc3c92456db550e41f943db44692c53e4d477
SlowNeutrino42 <PatrickConrad@gmx.de>
2015-08-24 19:02:22 +0200

    Added Classes: Log / ListRepository

---
+++ app/src/main/java/com/example/fabian/todolist/ListRepository.java
@@ -0,0 +1,7 @@
+package com.example.fabian.todolist;
+
+/**
+ * Created by Patrick on 24.08.2015.
+ */
+public class ListRepository  {
+}
---
+++ app/src/main/java/com/example/fabian/todolist/Log.java
@@ -0,0 +1,22 @@
+package com.example.fabian.todolist;
+
+/**
+ * Created by Patrick on 24.08.2015.
+ */
+public class Log {
+    public static final boolean DEBUG = true;
+    public static final String TAG = "AdvToDoList:";
+
+    public static void d(String tag, String msg) {
+        if (DEBUG) {
+            android.util.Log.d(tag, msg);
+        }
+    }
+
+    public static void d(String msg) {
+        if (DEBUG) {
+            android.util.Log.d(TAG, msg);
+        }
+    }
+}
+
